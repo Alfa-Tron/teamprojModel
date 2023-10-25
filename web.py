6b3395801a9e8a030d4b572292167c70f7a601fa
sedirov07 <sedirov07@mail.ru>
2023-10-25 18:26:56 +0500

    Добавлена реализация в streamlit

---
+++ web.py
@@ -0,0 +1,15 @@
+import io
+import streamlit as st
+from main import translator
+
+
+# Заголовок страницы
+st.title("Переводчик текста (RU-EN)")
+
+# Форма для ввода текста
+user_input = st.text_input("Введите ваш текст:")
+
+print(user_input)
+if user_input:
+    # Отображение введенного текста
+    st.write(f"Перевод: {translator(user_input)[0]['translation_text']}")
